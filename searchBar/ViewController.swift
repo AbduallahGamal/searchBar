585eed60bf0c6bfd02047274b35e684fc28f8d79
Abduallah <abduallahgamal@gmail.com>
2019-09-23 01:29:07 +0200

    Altering of Image

--- searchBar/ViewController.swift
+++ searchBar/ViewController.swift
@@ -68,7 +68,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 180
+        return 250
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
